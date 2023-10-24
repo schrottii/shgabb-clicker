9a2eaf68795dba3ffa3a4e5572b4c3c84d68fafb
fn78 <oh.seyy@yahoo.de>
2023-10-24 22:28:57 +0200

    selection buttons render

--- code/render.js
+++ code/render.js
@@ -1,6 +1,6 @@
-// Game made by Schrottii - editing or stealing is prohibited!
+﻿// Game made by Schrottii - editing or stealing is prohibited!
 
-// sel 1: shgabb - sandwiches - gs - silicone - am�
+// sel 1: shgabb - sandwiches - gs - silicone - amé
 // sel 2: gems - artifacts - shgic
 // sel 3: cheats - stats - achievements - other(social, patch notes)
 
@@ -22,4 +22,36 @@ var sections = {
     stats: document.getElementById("statsSection"),
     achievements: document.getElementById("achievementsSection"),
     social: document.getElementById("socialSection"),
+
+    // sels
+    selection1: document.getElementById("selection1"),
+    selection2: document.getElementById("selection2"),
+    selection3: document.getElementById("selection3"),
 }
+
+function renderSelection(sel) {
+    let render = ``;
+    let sels = [];
+
+    if (sel == 1) {
+        sels = ["shgabb", "sandwich", "goldenShgabb", "siliconeShgabb", "ameliorer"];
+        selsDisplay = ["Shgabb", "Sandwich", "Golden Shgabb", "Silicone Shgabb", "Améliorer"];
+    }
+    if (sel == 2) {
+        sels = ["gems", "artifacts", "minigames"];
+        selsDisplay = ["Gems", "Artifacts", "Minigames"];
+    }
+    if (sel == 3) {
+        sels = ["cheats", "stats", "achievements", "social"];
+        selsDisplay = ["Cheats", "Stats", "Achievements", "Social"];
+    }
+
+    for (s in sels) {
+        render = render + `<button class="settingButton" onclick="selectionRender(` + sel + `,` + sels[s] + `)">` + selsDisplay[s] + `</button>`
+    }
+    sections["selection" + sel].innerHTML = render;
+}
+
+renderSelection(1);
+renderSelection(2);
+renderSelection(3);
